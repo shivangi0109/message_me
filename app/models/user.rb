383d424cbe4c402086662f3f160fb3b681533f34
Shivangi Srivastava <shi06dec@gmail.com>
2021-04-19 18:10:45 -0600

    Build message resource

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
-  validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 15 }
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 15 }
   has_secure_password
+  has_many :messages
 end
